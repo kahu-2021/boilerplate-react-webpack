43dc13fd9e3f7f7e6db19bbad620c102903e083e
Isaac Sedcole <44042755+Isaac-Sedcole@users.noreply.github.com>
2021-02-03 11:27:11 +1300

    displaying random activities correctly

--- client/components/Random.jsx
+++ client/components/Random.jsx
@@ -1,39 +1,50 @@
-import React, {useState, useEffect} from 'react'
-import {getRandom} from '../apis/bored'
-
-
-
-
+import React, { useState, useEffect } from "react";
+import { getRandom } from "../apis/bored";
 
 const Random = () => {
-  const [activities, setActivities] = useState(null)
+  const [activities, setActivities] = useState(null);
 
-  useEffect( () => {
-    fetchActivity ()
+  useEffect(() => {
+    fetchActivity();
+  }, []);
 
-  } , [])
+  const [showInfo, setShowInfo] = useState(false);
 
-  const [showInfo, setShowInfo] = useState(false)
+  // const changeInfo = () => {
+  //   setShowInfo(!showInfo);
+  // };
 
-  const changeInfo = () => {
-    setShowInfo (!showInfo)
+  const fetchActivity = () => {
+    getRandom().then((fetchedActivity) => {
+      setActivities(fetchedActivity);
+    });
+  };
+
+  const bigFunc = () => {
+    if (!showInfo) {
+      setShowInfo(true);
+    }
+    getRandom().then((fetchedActivity) => {
+      setActivities(fetchedActivity);
+    });
+  };
+
+  const displayActivity = () => {
+    let properties = []
+    for(const property in activities) {
+      // console.log(property)
+      properties.push(<p> {property}: {activities[property]}</p>)
+    }
+    return properties
   }
-
-
-const fetchActivity = () => {
-  getRandom()
-  .then(fetchedActivity => {
-    setActivities(fetchedActivity)
-  })
-}
 
   return (
     <>
-    <h1>React development has begun!</h1>
-    <button onClick={fetchActivity, changeInfo}>Get Activity</button>
-    {(showInfo && activities.activity)}
+      <h1>Get a random activity</h1>
+      <button onClick={bigFunc}>Get Random Activity</button>
+      {showInfo &&  displayActivity()}
     </>
-  )
-}
+  );
+};
 
-export default Random
+export default Random;

eed802b62fab0da03614b9e9b44598d9cb0cf812
Lukas <63496113+lukas-jj@users.noreply.github.com>
2021-02-03 14:56:49 +1300

    no two characters are the same now

--- client/components/CharacterListing.jsx
+++ client/components/CharacterListing.jsx
@@ -1,55 +1,54 @@
-import React, { useEffect, useState } from 'react'
-import { getCharacterById, getCharacters } from '../apis/futurama'
+import React, { useEffect, useState } from "react"
+import { getCharacters } from "../apis/futurama"
 
 const CharacterListing = () => {
-  const [characters, setCharacters] = useState([])
   const [randomCharacter, setRandomCharacter] = useState({})
   const [randomCharacterTwo, setRandomCharacterTwo] = useState({})
   const [score, setScore] = useState(0)
 
   const fetchCharacters = () => {
-    getCharacters()
-      .then(fetchedCharacters => {
-        console.log(fetchedCharacters)
-        const randomNumber = () => Math.floor(Math.random() * Math.floor(fetchedCharacters.length))
-        let ranNum = randomNumber()
-        let ranNum2 = randomNumber()
+    getCharacters().then((fetchedCharacters) => {
+      console.log(fetchedCharacters)
+      const randomNumber = () =>
+        Math.floor(Math.random() * Math.floor(fetchedCharacters.length))
+      let ranNum = randomNumber()
+      let ranNum2 = randomNumber()
+      
+      // setRandomCharacter(fetchedCharacters[ranNum])
+      while (fetchedCharacters[ranNum].character == fetchedCharacters[ranNum2].character) {
+   
+        ranNum2 = randomNumber()
+      }
+      
+      setRandomCharacter(fetchedCharacters[ranNum])
+      setRandomCharacterTwo(fetchedCharacters[ranNum2])
 
-        while (ranNum === ranNum2) {
-          ranNum2 = randomNumber()
-        }
-
-        setRandomCharacter(fetchedCharacters[ranNum])
-        setRandomCharacterTwo(fetchedCharacters[ranNum2])
-      })
-  }
-
-  const fetchCharacterById = (id) => {
-    getCharacterById(id)
-      .then(fetchedCharacter => {
-        setSingleSatellite(fetchedCharacter)
-      })
+    })
   }
 
   useEffect(() => {
     fetchCharacters()
-
   }, [])
 
-   const handleClick = (character) => {
-    character == randomCharacter.character ?     setScore(prevScore => prevScore + 1)
-    : console.log("incorrect")
+  const handleClick = (character) => {
+    character == randomCharacter.character
+      ? setScore((prevScore) => prevScore + 1)
+      : console.log("incorrect")
     fetchCharacters()
   }
 
   return (
     <>
-    <h1>Quote: "{randomCharacter.quote}"</h1>
-    <h2>Who said it?</h2>
-      <button onClick={() => handleClick(randomCharacter.character)}>{randomCharacter.character}</button>
-      <button onClick={() => handleClick(randomCharacterTwo.character)}>{randomCharacterTwo.character}</button>
+      <h1>Quote: "{randomCharacter.quote}"</h1>
+      <h2>Who said it?</h2>
+      <button onClick={() => handleClick(randomCharacter.character)}>
+        {randomCharacter.character}
+      </button>
+      <button onClick={() => handleClick(randomCharacterTwo.character)}>
+        {randomCharacterTwo.character}
+      </button>
       <p>Score: {score}</p>
     </>
-  )
-}
-export default CharacterListing
+  );
+};
+export default CharacterListing;

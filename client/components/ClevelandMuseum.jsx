90ddeaa9eed521da057e8e6d0dcedc6c3f45125f
Julia Kurganskaia <17925683+julia-kurganskaia@users.noreply.github.com>
2021-02-04 11:22:45 +1300

    Add images

--- client/components/ClevelandMuseum.jsx
+++ client/components/ClevelandMuseum.jsx
@@ -9,28 +9,31 @@ const ClevelandMuseum = () => {
   const fetchArtworks = () => {
     getArtworks()
       .then(artworksList => {
-        console.log(artworksList)
-        setArtworks(artworksList)
-      })
+        // console.log(artworksList)
+        setArtworks(artworksList);
+      });
   }
   
   useEffect(() => {
       fetchArtworks();
     }, []
-  )
+  );
     
   return (
     <ul>
-      {artworks.data.map(artwork=> {
+      {artworks.data.filter(artwork => {
+          return artwork.images != null
+        }).map(artwork=> {
           return (
-            <li key={artwork.id}>
-              {artwork.title}
-            </li>
-          )
-        })
+              <li key={artwork.id}>
+                {artwork.title}
+                <img src={artwork.images.web.url} />
+              </li>
+            )
+          })
       }
     </ul>
-  )
+  );
 }
 
 export default ClevelandMuseum

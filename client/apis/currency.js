9d54ebb5838c7542024e65a50ae00617fa3cdcac
Rosemary Hawkes <rosemaryhawkes68@gmail.com>
2021-02-07 18:58:23 +1300

    Show real-time Bitcoin rate in NZD

--- client/apis/currency.js
+++ client/apis/currency.js
@@ -4,8 +4,8 @@ const baseUrl = 'https://api.coinbase.com/v2/exchange-rates'
 
 
 export function getAllRates() {
-    return request.get(baseUrl)//get a list of all bitcoin conversion rates
+    return request.get(baseUrl + '?currency=BTC')//get a list of all bitcoin conversion rates
         .then(res => res.body)
 }
 
-getAllRates().then(console.log)
+//getAllRates().then(console.log)
--- client/components/RatesListing.jsx
+++ client/components/RatesListing.jsx
@@ -3,29 +3,46 @@ import { getAllRates } from '../apis/currency'
 
 
 const RatesListing = () => {
-//Place to store retrieved rates
-const[rates, setRates] = useState([])
+//3=store data
+const[ratesData, setRatesData] = useState([])
+console.log(ratesData)
 
+//2=get data
 const fetchRates = () => {
     getAllRates()
         .then(fetchedRates => {
-            setRates(fetchedRates)
+            console.log(fetchedRates)
+            console.log("rates are fetched!")
+            setRatesData(fetchedRates)   
         })
 }
-//Initiate retrieval upon loading
+//1=Initiate retrieval upon loading of page
 useEffect(() => {
-    fetchRates
+    //console.log("Inside use effect function")
+    fetchRates()
+    //console.log("After fetch rates function called inside use effects")
 },[])
 
+
+
+//Json data is an object containing other objects. I want to generate jsx from array of objects. I want to reformat it as an array then map over to get NZD, AUD and USD values.  Object values function.
+
+let ratesArray = Object.values(ratesData)
+console.log(ratesArray)
+
   return (
-      <ul>
-          {rates.map(r => {
-            return (
-                <li>{rates.data}</li>
-            )
-          })}
-      </ul>
+      <>
+      <p>Show cost of 1 Bitcoin in NZD</p>
+         <ul>
+            {ratesArray.map(r => {
+             return (
+                 <li>{r.rates['NZD']}</li>
+             )
+           })}
+          </ul>
+      </>
     )
 }
 
 export default RatesListing
+
